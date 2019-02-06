a54af001870526bec40842e8f0115581c79a502f
Moses Olawoyin <m.bastford@gmail.com>
2019-02-06 00:07:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   minimum_key = nil
 name_hash.each do |key, value|
   if minimum_value == nil || value < minimum_value 
-    minimum = value
+    minimum_value = value
     minimum_key = key
   end
 end
